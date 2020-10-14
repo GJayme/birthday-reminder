38f054c0582c574cb8376719bcdb3bd2600b9d23
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-10-14 13:58:37 -0300

    feat: setup innitial project

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import data from './data/data';
+import List from './component/List';
+
+function App() {
+  return <h2>Reminder project setup</h2>;
+}
+
+export default App;
