c6d34fc54cf2e705e854bd9e1de318fb8f0067b5
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-10-14 14:12:43 -0300

    feat: using useState

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './data/data';
 import List from './component/List';
 
 function App() {
-  return <h2>Reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
